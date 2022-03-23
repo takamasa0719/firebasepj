6017744226532e637811008e595070422b350a90
takamasa0719 <theguitar74@gmail.com>
2022-03-23 18:48:39 +0900

    second

---
+++ plugins/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAxwkO8sdBBTjTdZWDCMR4Lekglj9Preeo",
+    authDomain: "fir-pj-217da.firebaseapp.com",
+    projectId: "fir-pj-217da",
+    storageBucket: "fir-pj-217da.appspot.com",
+    messagingSenderId: "414938768159",
+    appId: "1:414938768159:web:759c497f9692887e31462a",
+    measurementId: "G-FEKF4T15NX"
+  };
